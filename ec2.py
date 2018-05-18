a490c557f3298a51f4dd3f88100cfe55caf4d025
artemdevel <artem.devel2014@gmail.com>
2018-05-19 01:22:05 +0300

    Add NextToken support for EC2 utils

--- ec2.py
+++ ec2.py
@@ -10,33 +10,40 @@ def get_info():
     data = client.describe_instances()
     result = []
 
-    for reservation in data['Reservations']:
-        for instance in reservation['Instances']:
-            ec2_instance_id = instance.get('InstanceId')
-            ec2_public_ips = set()
-            ec2_public_ip6s = set()
-            ec2_public_dns_names = set()
+    while True:
+        next_token = data.get('NextToken')
+        for reservation in data['Reservations']:
+            for instance in reservation['Instances']:
+                ec2_instance_id = instance.get('InstanceId')
+                ec2_public_ips = set()
+                ec2_public_ip6s = set()
+                ec2_public_dns_names = set()
 
-            # PublicIpAddress and PublicDnsName exists for EC2-Classic
-            if instance.get('PublicIpAddress', False):
-                ec2_public_ips.add(instance.get('PublicIpAddress'))
-                ec2_public_dns_names.add(instance.get('PublicDnsName'))
+                # PublicIpAddress and PublicDnsName exists for EC2-Classic
+                if instance.get('PublicIpAddress', False):
+                    ec2_public_ips.add(instance.get('PublicIpAddress'))
+                    ec2_public_dns_names.add(instance.get('PublicDnsName'))
 
-            # NetworkInterfaces exists for EC2-VPC
-            for interface in instance.get('NetworkInterfaces', []):
-                if interface.get('Association', {}).get('PublicIp', False):
-                    ec2_public_ips.add(interface['Association']['PublicIp'])
-                    ec2_public_dns_names.add(interface['Association']['PublicDnsName'])
+                # NetworkInterfaces exists for EC2-VPC
+                for interface in instance.get('NetworkInterfaces', []):
+                    if interface.get('Association', {}).get('PublicIp', False):
+                        ec2_public_ips.add(interface['Association']['PublicIp'])
+                        ec2_public_dns_names.add(interface['Association']['PublicDnsName'])
 
-                for ip6 in interface.get('Ipv6Addresses'):
-                    ec2_public_ip6s.add(ip6['Ipv6Address'])
+                    for ip6 in interface.get('Ipv6Addresses'):
+                        ec2_public_ip6s.add(ip6['Ipv6Address'])
 
-            if ec2_public_ips:
-                result.append({
-                    'ec2_instance_id': ec2_instance_id,
-                    'ec2_public_ips': list(ec2_public_ips),
-                    'ec2_public_ip6s': list(ec2_public_ip6s),
-                    'ec2_public_dns_names': list(ec2_public_dns_names),
-                })
+                if ec2_public_ips:
+                    result.append({
+                        'ec2_instance_id': ec2_instance_id,
+                        'ec2_public_ips': list(ec2_public_ips),
+                        'ec2_public_ip6s': list(ec2_public_ip6s),
+                        'ec2_public_dns_names': list(ec2_public_dns_names),
+                    })
+
+        if next_token is None:
+            break
+
+        data = client.describe_instances(NextToken=next_token)
 
     return result
