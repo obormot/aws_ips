248e269cc8c9a30c2740fab1902b925b266e6308
artemdevel <artem.devel2014@gmail.com>
2018-05-19 12:31:41 +0300

    Add ELBv2 utils

--- aws_ips.py
+++ aws_ips.py
@@ -5,17 +5,19 @@ import elasticsearch
 import apigateway
 import cloudfront
 import elb
+import elbv2
 
 from utils import jprint
 
 
 def main():
-    # jprint(ec2.get_info())
-    # jprint(redshift.get_info())
-    # jprint(elasticsearch.get_info())
-    # jprint(apigateway.get_info())
-    # jprint(cloudfront.get_info())
+    jprint(ec2.get_info())
+    jprint(redshift.get_info())
+    jprint(elasticsearch.get_info())
+    jprint(apigateway.get_info())
+    jprint(cloudfront.get_info())
     jprint(elb.get_info())
+    jprint(elbv2.get_info())
 
 
 if __name__ == '__main__':
